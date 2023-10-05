0f1622ba17207b919be46b11b0d71da31abc8f78
Neerj Kumar <119673950+golubhaii@users.noreply.github.com>
2023-10-05 18:52:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,9 +37,10 @@ class Bot(Client):
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats        
-        await super().start()
+        temp.BANNED_CHATS = b_chats 
+         await super().start()
   File "/opt/render/project/src/.venv/lib/python3.7/site-packages/pyrogram/methods/utilities/start.py"
+        await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
         temp.U_NAME = me.username
