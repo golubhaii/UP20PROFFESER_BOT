11629d4e3b1d95a92a17a2be4f28760f5c03d95f
Neerj Kumar <119673950+golubhaii@users.noreply.github.com>
2023-10-07 18:00:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -304,7 +304,7 @@ def get_time(seconds):
     return result
     
 async def get_shortlink(link):
-    url = f'{SHORT_URL}/api'
+    url = f'paisakamalo.in/api'
     params = {'api': SHORT_API, 'url': link}
     try:
         async with aiohttp.ClientSession() as session:
